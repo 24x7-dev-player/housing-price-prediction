e1f70dbc0fe4a4daa04f20342e23e63b2c9fde23
talkativetushar <61559891+talkativetushar@users.noreply.github.com>
2020-05-28 07:30:07 +0530

    Add files via upload

---
+++ version.py
@@ -0,0 +1,29 @@
+import flask
+import gunicorn
+import jinja2
+import markupsafe
+import werkzeug
+import numpy
+import scipy
+import sklearn
+import pandas 
+import pickle
+import joblib
+import tensorflow
+import keras
+import google-auth
+print(flask.__version__)
+print(gunicorn.__version__)
+print(jinja2.__version__)
+print(markupsafe.__version__)
+print(werkzeug.__version__)
+print(numpy.__version__)
+print(scipy.__version__)
+print(sklearn.__version__)
+print(pandas.__version__)
+print(joblib.__version__)
+print(tensorflow.__version__)
+print(keras.__version__)
+print(google-auth.__version__)
+
+
